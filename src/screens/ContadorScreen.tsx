3dee4bb9f4d9e2d9192df2bee673c8af1d650564
Jhon D Orozco <jhon.orozco@javerianacali.edu.co>
2022-11-09 15:17:04 -0500

    [ADD] Boton personalizado flotando

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -12,9 +12,15 @@ export const ContadorScreen = () => {
                 Contador: {contador} 
             </Text>
 
-            <TouchableOpacity onPress={() => setcontador( contador + 1)}>
-                <View style={ styles.botonIncrementar }>
-                    <Text>+1</Text>
+            <TouchableOpacity style={styles.fabLocationBR} onPress={() => setcontador( contador + 1)}>
+                <View style={styles.fab}>
+                    <Text style={styles.fabText}>+1</Text>
+                </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.fabLocationBL} onPress={() => setcontador( contador - 1)}>
+                <View style={styles.fab}>
+                    <Text style={styles.fabText}>-1</Text>
                 </View>
             </TouchableOpacity>
         </View>
@@ -27,12 +33,34 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
     }, title: {
-        textAlign: 'center',
         fontSize: 40,
         top: -15,
+        textAlign: 'center',
     },
-    botonIncrementar: {
-        backgroundColor: 'red',
+
+    fabLocationBR:{
+        position: 'absolute',
+        bottom: 25,
+        right: 25
+    },
+
+    fabLocationBL:{
+        position: 'absolute',
+        bottom: 25,
+        left: 25
+    },
+
+    fab:{
+        backgroundColor:'#5856D6',
+        width: 60,
+        height: 60,
         borderRadius: 100,
+        justifyContent: 'center',
+    },
+    fabText:{
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+        alignSelf: 'center'
     }
 })
