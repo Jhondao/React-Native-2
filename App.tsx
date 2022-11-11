c98f1f80363099c5aebddbbc4b2d195bb3329de0
Jhon Orozco <jhondao.desarrollo@hotmaile.com>
2022-11-11 17:31:06 -0500

    [ADD] Position

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { DimensionesScreen } from './src/screens/DimensionesScreen';
+// import { DimensionesScreen } from './src/screens/DimensionesScreen';
 // import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 // import { ContadorScreen } from './src/screens/ContadorScreen';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+import { PositionScreen } from './src/screens/PositionScreen';
 
 export const App = () => {
   return (
@@ -12,7 +13,9 @@ export const App = () => {
       {/* <HolaMundoScreen /> */}
       {/* <ContadorScreen/> */}
       {/* <BoxObjectModelScreen/> */}
-      <DimensionesScreen />
+      {/* <DimensionesScreen /> */}
+      <PositionScreen />
+
     </SafeAreaView>
 
   );
